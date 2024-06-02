7a5cf3f8b5d05e99e933d6f452ffbfb6131ddf65
Ivirton <ivirton23@gmail.com>
2024-06-01 21:42:58 -0300

    ultimo comit

--- src/controller/timesController.js
+++ src/controller/timesController.js
@@ -1,12 +1,16 @@
 const database = require("../config/database");
-const { Time, Jogador, Categoria } = require("../model/models");
+const { Time, Jogador, Categoria, Transmissao } = require("../model/models");
 
 const timeController = {
     find: async (req, res) => {
         try {
+            const query = `
+            select Jogo.idjogo, jogo.id_equipe1,jogo.id_equipe2,Jogo.data, time1.nome  as nome_time1, pontos_time1, time2.nome as nome_time2 ,pontos_time2,estadio.nome as estadio,Categoria.nome as nomeCategoria,Categoria.id_categoria,Categoria.sexo  from Jogo ,Times AS time1,Times as time2,estadio,Categoria WHERE Jogo.id_equipe1 = time1.id_equipe and time2.id_equipe = Jogo.id_equipe2 and estadio.idestadio = Jogo.idestadio and Categoria.id_categoria = Jogo.id_categoria and (id_equipe1 = ${req.query.id} OR id_equipe2 = ${req.query.id})  order by jogo.data ASC;`
+            const transmisoes = await Transmissao.findAll()
             const time = await Time.findByPk(req.query.id)
             const jogadores = await Jogador.findAll({ where: { id_equipe: req.query.id } })
-            res.render('time', { time: time, jogadores: jogadores, categorias: await Categoria.findAll({ order: [['nome', 'ASC']] }) })
+            const jogos = await database.query(query)
+            res.render('time', { transmisoes: transmisoes,jogos: jogos[0],time: time, jogadores: jogadores, categorias: await Categoria.findAll({ order: [['nome', 'ASC']] }) })
         } catch (error) {
             console.log(error)
         }
@@ -36,11 +40,12 @@ const timeController = {
                 nome: nomeTime.toLocaleUpperCase(),
                 id_categoria: id_categoria
             })
-            console.log('Time adicionado :', novoTime.toJSON());
+            res.redirect('/time?id='+novoTime.id_equipe)
         } catch (erro) {
-            console.error('Erro ao adicionar o time:', error);
+            console.error('Erro ao adicionar o time:', erro);
+            res.redirect('/times')
         }
-        res.redirect('/times')
+       
     },
     delete: async (req, res) => {
         try {
